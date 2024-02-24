ae2e33cf9d503c7f4660df0a50c0db0c9b174784
tony0001234 <tony0001234@gmail.com>
2024-02-23 18:38:01 -0600

    Terminado hasta el inciso 4, pensando en convertir de romano a decimal

--- tarea_02/tarea_02.py
+++ tarea_02/tarea_02.py
@@ -1,4 +1,5 @@
 from os import system, startfile #importo os para utilizar funcionalidades dependientes del sistema operativo, en este caso el system("cls")
+from math import sqrt
 
 def menu():
     print()
@@ -26,13 +27,23 @@ def convertir_a_binario(numeroABin):
         return "1"
     else:
         return convertir_a_binario(numeroABin // 2) + str(numeroABin % 2)
+    
+def contar_digitos(numeroCD):
+    if(abs(numeroCD) < 10):
+        return 1
+    else:
+        return 1 + contar_digitos(abs(numeroCD) // 10)
 
-def es_entero(numero):
-    try:
-        valorI = int(numero)
-        return True, valorI
-    except ValueError:
-        return False, None
+def raiz_cuadrada_entera(numeroEn):
+        if(numeroEn < 0):
+            print("Error: Debes utilizar numeros positivos para raiz cuadrada")
+        else:
+            print(f"La raiz cuadrada aproximada al entero mas cercano de: {numero3} Es: {calcular_raiz_cuadrada(numero3)}")
+
+def calcular_raiz_cuadrada(numeroRC):
+    return round(sqrt(numeroRC))
+
+
 
 while True:
 
@@ -47,7 +58,37 @@ while True:
             numeroConv = convertir_a_binario(numeroIn)
             print(f"El numero: {numeroIn} En binario es: {numeroConv}")
         except ValueError as e:
-            print("Error: porfavor ingrese un valor valido")
+            print("Error: Porfavor ingrese un valor valido")
+            print(f"Error: {e}")
+    
+    if(opc == "2"):
+        system("cls")
+        try:
+            numero2 = int(input("Ingrese un numero entero para calcular sus digitos: "))
+            numeroDigi = contar_digitos(numero2)
+            print(f"La cantidad de digitos del numero: {numero2} Es: {numeroDigi}")
+        except ValueError as e:
+            print("Error: Porfavor ingrese un valor valido")
+            print(f"Error: {e}")
+
+    if(opc == "3"):
+        numero3 = 0
+        system("cls")
+        try:
+            numero3 = float(input("Ingrese un numero positivo para calcularla raiz cuadrada entera: "))
+            raiz_cuadrada_entera(numero3)
+        except ValueError as e:
+            print("Error: Porfavor ingrese un valor valido")
+            print(f"Error: {e}")
+            
+    if(opc == "4"):
+        system("cls")
+        try:
+            numeroRom = str(input("Ingrese el numero Romano que desea convertir: "))
+
+
+        except ValueError as e:
+            print("Error: Porfavor ingrese un valor valido")
             print(f"Error: {e}")
 
     elif(opc == "6"):
