e791993d5db37353b3f20a23c2a384d08bb7cdca
tony0001234 <tony0001234@gmail.com>
2024-04-11 18:52:27 -0600

    borrado host y puerto

--- Hoja de trabajo 4/AVL_API.py
+++ Hoja de trabajo 4/AVL_API.py
@@ -102,7 +102,7 @@ class ABB:
 
 
         if __name__ == "__main__":
-            app.run(debug=True, host='0.0.0.0', port=3000)
+            app.run(debug=True)
 
     ''''''
     def insert(self, valor, nombre, DPI):
