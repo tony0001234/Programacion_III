b7a3207e41a8534e8bf93c446decfe89be022775
tony0001234 <tony0001234@gmail.com>
2024-04-11 19:52:22 -0600

    probando cambiar comillas

--- Hoja de trabajo 4/AVL_API.py
+++ Hoja de trabajo 4/AVL_API.py
@@ -271,7 +271,7 @@ def index():
 def get_all_opciones():
     return jsonify(Opciones)
 
-@app.route('/api/o1/cargaMasCSV/', methods=("POST"))########opcion 1
+@app.route("/api/o1/cargaMasCSV/", methods=("POST"))########opcion 1
 def carga_archivo_CSV():
     try:   
         arbol.agregarArch()
